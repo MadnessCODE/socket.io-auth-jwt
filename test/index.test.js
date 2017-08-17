cd9ee12790e8d6bf384d22aaceb132a58b1b668a
madnesscode <madness.studio@gmail.com>
2017-08-17 20:45:42 +0200

    fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -12,34 +12,34 @@ describe('authorizer', function () {
         it('and token is missing', function (done) {
             var socket = io('http://localhost:3000', {query: "token="});
 
-            socket.on("unauthorized", (data) => {
+            socket.on("unauthorized", function(data) {
                 assert.equal(data, 'jwt must be provided');
                 done();
             });
         });
 
-        it('and token is invalid', (done) => {
+        it('and token is invalid', function(done) {
             var socket = io('http://localhost:3000', {query: "token="+555});
 
-            socket.on("unauthorized", (data) => {
+            socket.on("unauthorized", function(data) {
                 assert.equal(data, 'jwt malformed');
                 done();
             });
         });
 
-        it('and token signature is wrong', (done) => {
+        it('and token signature is wrong', function(done) {
             var socket = io('http://localhost:3000', {query: "token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"});
 
-            socket.on("unauthorized", (data) => {
+            socket.on("unauthorized", function(data) {
                 assert.equal(data, 'invalid signature');
                 done();
             });
         });
 
-        it('and token is expired', (done) => {
+        it('and token is expired', function(done) {
             var socket = io('http://localhost:3000', {query: "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUwMjk5MTY0NCwiZXhwIjoxNTAyOTkxNjQ1fQ.41V8o-Y0ue35zPoBS72EPt1wn3LjDxp_BSMzBBCF8-U"});
 
-            socket.on("unauthorized", (data) => {
+            socket.on("unauthorized", function(data) {
                 assert.equal(data, 'jwt expired');
                 done();
             });
